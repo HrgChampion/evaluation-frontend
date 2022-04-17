2f3d1d6e24c2fb4b1da84d0e4668f61b35e9323e
Himanshu <hgauba4@gmail.com>
2022-04-17 19:55:19 +0530

    done

--- flat-project/src/Components/Residents/Residents.jsx
+++ flat-project/src/Components/Residents/Residents.jsx
@@ -5,16 +5,16 @@ import { useParams } from "react-router-dom";
 export const Resident=()=>{
 
     const id =useParams().id;
-    const [residents,setResidents]=useState([]);
-    useEffect((id)=>{
+    const [residents,setResidents]=useState({});
+    useEffect(()=>{
        
-        getData(id);
+        getData();
     },[]);
-    const getData=(id)=>{
+    const getData=()=>{
         axios.get( `http://localhost:2345/flats/flat/${id}`)
         .then(res=>{
-            //setResidents(res.data);
-            console.log(res.data)
+            setResidents(res.data.Residents);
+            
         })
         .catch(err=>{
             console.log(err);
@@ -25,15 +25,16 @@ export const Resident=()=>{
     return(
         <div>
             <h1>Residents</h1>
-            <div>{id}</div>
-            {/* {residents.map(resident=>{
-                return(
+            
                     <div>
-                        <div>{resident.Name}</div>
-                        <div>{resident.flat_id}</div>
+                        <div>{residents.Name}</div>
+                        <div>{residents.Flat_Owner}</div>
+                        
+                        <div>{residents.Residents}</div>
+                       
                     </div>
-                )
-            })} */}
+               
+           
             
         </div>
     )
